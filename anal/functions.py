35cd9c2a4c8e3bb32e81e95fb96bef9f12d74fd2
usscltd <usscltd@gmail.com>
2018-05-10 14:27:39 +0500

    get symbols optimized

--- anal/functions.py
+++ anal/functions.py
@@ -13,13 +13,12 @@ if len(argv) < 2:
 def enum(r2):
 	global symbols
 	modulename = r2.cmd('iV~Internal[1]')
-	for fcn in json.loads( r2.cmd('aflj') ):
+	for fcn in json.loads( r2.cmd('aflj') or '{}' ):
 		try:
-			r2.cmd('s %s' % fcn)
 			name = fcn['name']
 			start = int( fcn['minbound'] )
 			end = int( fcn['maxbound'] )
-			nargs = int( r2.cmd('afi~args[1]') or 0 )
+			nargs = int( fcn['nargs'] )
 			print Fore.LIGHTGREEN_EX + "[+] %s!%s 0x%x 0x%x %d" % (modulename, name, start, end, nargs) + Fore.RESET
 			symbols.writerow( [modulename, name, start, end, nargs] )
 		except Exception as e:
@@ -27,9 +26,9 @@ def enum(r2):
 
 def anal(filepath):
 	r2 = r2pipe.open(filepath)
-	r2.cmd("aa")
-	r2.cmd("aar")
-	r2.cmd("aac")
+	r2.cmd("af @@ sym.*")
+	#r2.cmd("aar")
+	#r2.cmd("aac")
 	enum(r2)
 
 symbols = csv.writer( open('symbols.csv','a'), delimiter=',' )
