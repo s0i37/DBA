a225acd4df9d7eea194b9da1ad7813cf32eabda7
usscltd <usscltd@gmail.com>
2018-05-11 18:15:12 +0500

    added from,to keys

--- graphic.py
+++ graphic.py
@@ -1,89 +1,129 @@
 from PIL import Image, ImageDraw, ImageFont
 from random import random
-from sys import argv
-import sqlite3
+from sys import argv,stdout
+import argparse
+import os
+import csv
+from colorama import Fore
 
 WIDTH = 4096
 HEIGHT = 3072
-min_bound = 0x0
-max_bound = 0x80000000
 
-if len(argv) != 3:
-	print "%s trace.txt symbols.db" % argv[0]
-	exit()
+modules = {}
+symbols = {}
+
+parser = argparse.ArgumentParser( description='execution trace visualization tool' )
+parser.add_argument("tracefile", type=str, help="trace.txt")
+parser.add_argument("symbols", nargs='?', default='', help="symbols.csv")
+parser.add_argument("-from_addr", type=int, default=0, help="from address")
+parser.add_argument("-to_addr", type=int, default=0x7fffffff, help="to address")
+parser.add_argument("-modules", nargs="+", help="draw only specified modules")
+parser.add_argument("-from_takt", type=int, default=0, help="from takt")
+parser.add_argument("-to_takt", type=int, default=0, help="to takt")
+args = parser.parse_args()
 
 def get_module(eip):
 	global modules
-	for module,bounds in modules.items():
-		if bounds[0] <= eip <= bounds[1]:
-			return module
+	for (modulename,_range) in modules.items():
+		(start,end) = _range
+		if start <= eip <= end:
+			return { 'name': modulename, 'start': start, 'end': end }
+	return {}
 
-trace_file = argv[1]
-symbols_db = argv[2]
-db = sqlite3.connect(symbols_db)
-sql = db.cursor()
+def get_symbol(eip):
+	global symbols
+	for (symbol,_range) in symbols.items():
+		(start,end) = _range
+		if start <= eip <= end:
+			return { 'name': symbol, 'start': start, 'end': end }
+	return {}
 
-modules = {}
-for info in sql.execute( 'select * from modules' ).fetchall():
-	module,start,end = info
-	modules.update( { module: [ int(start), int(end) ] } )
+if os.path.isfile(args.symbols):
+	for (modulename, symbol, start, end, nargs) in csv.reader( open(args.symbols, 'r'), delimiter=',' ):
+		start = int(start, 16)
+		end = int(end, 16)
+		symbols[symbol] = [start, end]
+		if modules.get(modulename):
+			if start < modules[modulename][0]:
+				modules[modulename][0] = start
+			if end > modules[modulename][1]:
+				modules[modulename][1] = end
+		else:
+			modules[modulename] = [start, end]
 
+for (modulename,_range) in modules.items():
+	print Fore.LIGHTBLACK_EX + "%s 0x%08x 0x%08x" % (modulename, _range[0], _range[1]) + Fore.RESET
 
 img = Image.new( 'RGB', (WIDTH,HEIGHT), "white" )
 draw = ImageDraw.Draw(img)
 pixels = img.load()
         
 eips = []
-instr_count = 0
-function_names = []
-functions_regions = []
-functions_bounds = {}
+takt = 0
 
-
-#def get_func_bound(addr):
-#	func = get_func(addr)
-#	if func:
-#		return ( func.startEA, func.endEA )
-#	else:
-#		print hex(addr)
-#	return (0,0)
-
-modules_used = []
-with open( trace_file ) as f:
+modules_used = {}
+symbols_used = {}
+module = {}
+symbol = {}
+with open( args.tracefile ) as f:
 	for line in f:
 		eip = line.split()[1][1:11]
 		eip = int(eip, 16)
-		if min_bound <= eip <= max_bound:
-			
-			#if not eip in functions_regions:
-			#	function_name = GetFunctionName(eip)
-			#	if not function_name in function_names:
-			#		function_names.append(function_name)
-			#		function_bounds = get_func_bound(eip)
-			#		functions_regions.extend( xrange( *function_bounds ) )
-			#		functions_bounds[function_name] = function_bounds
+		takt += 1
 
-			eips.append(eip)
-			module = get_module(eip)
-			if module and not module in modules_used:
-				modules_used.append(module)
-			instr_count += 1
-		if instr_count and instr_count % 100000 == 0:
-			print instr_count
+		if args.from_addr == 0 and args.to_addr == 0 or args.from_addr <= eip <= args.to_addr:
+			if args.from_takt == 0 or args.from_takt <= takt:
+				if not ( module and module['start'] <= eip <= module['end'] ):
+					module = get_module(eip)
+					if module and not module['name'] in modules_used.keys():
+						modules_used[ module['name'] ] = [ module['start'], module['end'] ]
 
-min_eip = min(eips)
-max_eip = max(eips)
+					if not ( symbol and symbol['start'] <= eip <= symbol['end'] ):
+						symbol = get_symbol(eip)
+						if symbol and not symbol['name'] in symbols_used.keys():
+							symbols_used[ symbol['name'] ] = [ symbol['start'], symbol['end'] ]
+
+				if args.modules and module and not module['name'] in args.modules:
+					pass
+				else:
+					eips.append(eip)
+
+		if takt and takt % 10000 == 0:
+			stdout.write( "\r%d/%d %s %s" % ( len(eips), takt, module.get('name') or '', symbol.get('name') or '' ) )
+			stdout.flush()
+
+		if args.to_takt and takt > args.to_takt:
+			break
+
+
+if args.modules:
+	min_eip = min( map( lambda m: modules[m][0], args.modules) )
+	max_eip = max( map( lambda m: modules[m][1], args.modules) )
+else:
+	min_eip = min(eips)
+	max_eip = max(eips)
+
 y_scale = float(max_eip - min_eip)/(HEIGHT-1)
-x_scale = float(instr_count)/(WIDTH-1)
+x_scale = float(takt - args.from_takt)/(WIDTH-1)
 
 
-for module in modules_used:
-	bounds = modules[module]
-	draw.rectangle( ( ( 0, int((bounds[0]-min_eip)/y_scale) ), ( WIDTH, int((bounds[1]-min_eip)/y_scale) ) ), fill=( 0, 200+int(random()*(0xff-200)), 200+int(random()*(0xff-200)) ) )
+for modulename, _range in modules_used.items():
+	(start,end) = _range
+	draw.rectangle( ( ( 0, int( (start-min_eip)/y_scale ) ), ( WIDTH, int( (end-min_eip)/y_scale ) ) ), fill=( 0, 200, 200+int(random()*(0xff-200)) ) )
+	draw.text( (0, int( (start-min_eip)/y_scale) ), modulename, 'black', font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12))
 
-for module in modules_used:
-	bounds = modules[module]
-	draw.text( (0, int((bounds[0]-min_eip)/y_scale)), module, 'black', font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12))
+for symbolname, _range in symbols_used.items():
+	(start,end) = _range
+	draw.rectangle( ( ( 0, int( (start-min_eip)/y_scale ) ), ( WIDTH, int( (end-min_eip)/y_scale ) ) ), fill=( 0, 200+int(random()*(0xff-200)), 200 ) )
+	draw.text( (0, int( (start-min_eip)/y_scale) ), symbolname, 'black', font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12))
+
+for _takt in xrange( args.from_takt, takt, (takt - args.from_takt)/10 ):
+	draw.line( ( int(_takt/x_scale), 0, int(_takt/x_scale), HEIGHT ), fill=(0,0,0) )
+	draw.text( ( int(_takt/x_scale), 10 ), str(_takt), 'black', font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12))
+
+for _addr in xrange( min_eip, max_eip, (max_eip - min_eip)/10 ):
+	draw.line( ( 0, int(_addr/y_scale), WIDTH, int(_addr/y_scale) ), fill=(0,0,0) )
+	draw.text( ( 0, int(_addr/y_scale) ), "0x%08x"%_addr, 'black', font=ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12))
 
 
 i = 0
