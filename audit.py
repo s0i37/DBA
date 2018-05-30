b490eb9e447772bae080c80b173d159c12ed7961
s0i37 <s0i37@work>
2018-05-30 19:23:51 +0500

    without important changes

--- audit.py
+++ audit.py
@@ -2,8 +2,14 @@ from sanlib.emulate import execute
 from sanlib.sanitizers import MemoryLeak, UMR_stack, UAR, OOB_read_stack, OOB_read_heap, OOB_write_heap, UAF, DoubleFree, UMR_heap, UMR_stack, UWC, Exceptions, SoF
 import sys
 
-MALLOC = 0x080483a0
-FREE = 0x08048380
+MALLOC = 0x00403f94
+FREE = 0x56580490
+
+'''
+TODO:
+	malloc(arg0)/HeapAlloc(arg2)
+	x86/x64
+'''
 
 memoryleak = MemoryLeak(malloc_ptr=MALLOC, free_ptr=FREE)
 oob_read_heap = OOB_read_heap(malloc_ptr=MALLOC, free_ptr=FREE)
@@ -13,15 +19,17 @@ doublefree = DoubleFree(malloc_ptr=MALLOC, free_ptr=FREE)
 urm_heap = UMR_heap(malloc_ptr=MALLOC, free_ptr=FREE)
 uwc = UWC(malloc_ptr=MALLOC, free_ptr=FREE)
 
-#sanitizers = [ UAR(), oob_read_heap, oob_write_heap, uaf, doublefree, urm_heap, uwc, Exceptions(), SoF() ]
-sanitizers = [ memoryleak ]
-#sanitizers_bad = [UMR_stack(), OOB_read_stack()]
-#sanitizers_not_impl = [MemoryLeak(), OOB_write_stack()]
+#sanitizers_impl = [ UAR(), oob_read_heap, oob_write_heap, uaf, doublefree, urm_heap, uwc, Exceptions(), SoF() ]
+sanitizers_non_heap = [ UAR(), SoF() ]
+#sanitizers_heap = [ oob_read_heap, oob_write_heap, uaf, doublefree, urm_heap, uwc ]
+#sanitizers_not_impl = [ memoryleak ]
+#sanitizers_bad = [ UMR_stack(), OOB_read_stack(), Exceptions() ]
+#sanitizers_not_impl = [ MemoryLeak(), OOB_write_stack() ]
 trace_file = sys.argv[1]
 
 with open(trace_file) as trace:
 	for line in trace:
 		result = execute(line)
 		if result:
-			for sanitizer in sanitizers:
+			for sanitizer in sanitizers_non_heap:
 				sanitizer(*result)
