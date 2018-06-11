4025760de45632b8c9b9c5c822b9d9e189a5e9a9
soier <soier@home>
2018-06-12 02:36:14 +0500

    taint engine 5 time acceleration

--- taint.py
+++ taint.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from lib.emulate import execute, CPU
+from lib.emulate import Trace, CPU
 import argparse
 import colorama
 
@@ -12,19 +12,20 @@ parser.add_argument("-from_addr", type=int, default=0, help="print tainted instr
 parser.add_argument("-to_addr", type=int, default=0, help="print tainted instruction only to address")
 parser.add_argument("-from_takt", type=int, default=0, help="print tainted instruction only after takt")
 parser.add_argument("-to_takt", type=int, default=0, help="print tainted instruction only before takt")
+parser.add_argument("-n", dest= "limit", type=int, default=0, help="count of print tainted instructions")
 parser.add_argument("-v", dest="verbose", type=bool, default=False, help="verbose")
 args = parser.parse_args()
 
 tainted_regs = set()
 tainted_mems = set()
 
-def find_string(addr, string):
+def find_string(memory, addr, string):
 	was_found = False
 	low_boundary_search = addr - len(string)
 	i = 0
 	while addr >= low_boundary_search:
 		for i in xrange( len(string) ):
-			value = cpu.cache.get_byte(addr + i)
+			value = memory.get_byte(addr + i)
 			if value == None:
 				return None
 			if string.find( chr(value) ) == -1:
@@ -38,7 +39,7 @@ def find_string(addr, string):
 	return None
 
 
-def taint(used_registers, used_memory):
+def taint(used_registers, used_memory, memory):
 	global tainted_regs, tainted_mems
 	taint_regs = set()
 	taint_mems = set()
@@ -49,8 +50,7 @@ def taint(used_registers, used_memory):
 
 	if args.taint_data:
 		for used_memory_cell in used_mems_r:
-			#print "%d %s 0x%08x %d" % (cpu.takt, cpu.instruction, used_memory_cell, len(args.taint_data))
-			string_ptr = find_string(used_memory_cell, args.taint_data)
+			string_ptr = find_string(memory, used_memory_cell, args.taint_data)
 			if string_ptr:
 				is_spread = True
 				for ptr in xrange(string_ptr, string_ptr+len(args.taint_data)):
@@ -116,23 +116,27 @@ if not tainted_mems and tainted_regs:
 	exit()
 
 taint_no = 0
-with open(args.tracefile) as trace:
-	for line in trace:
-		result = execute(line)
-		if not result:
+with Trace( open(args.tracefile) ) as trace:
+	while True:
+		info = trace.instruction() # 5 time faster
+		if not info:
 			continue
-		(cpu, used_registers, used_memory) = result
-		if args.from_takt <= cpu.takt:
-			(taint_regs, taint_mems) = taint(used_registers, used_memory)
+
+		(used_registers, used_memory) = info
+		if args.from_takt <= trace.cpu.takt:
+			(taint_regs, taint_mems) = taint(used_registers, used_memory, trace.io.ram)
 			if taint_regs or taint_mems:
-				if (args.from_addr == 0 and args.to_addr == 0) or args.from_addr <= cpu.eip_before <= args.to_addr:
+				if (args.from_addr == 0 and args.to_addr == 0) or args.from_addr <= trace.cpu.eip_before <= args.to_addr:
 					taint_no += 1
-					print colorama.Fore.LIGHTGREEN_EX + "[+] %d:%d:0x%08x: %s ;" % (taint_no, cpu.takt, cpu.eip_before, cpu.instruction) + colorama.Fore.GREEN,
-					for taint_reg in taint_regs:
-						print " %s=0x%08x," % ( taint_reg, cpu.get(taint_reg) ),
-					for taint_mem in taint_mems:
-						print " 0x%08x -> 0x%08x," % ( taint_mem, cpu.cache.get_dword(taint_mem) ),
-					print colorama.Fore.RESET
+					if args.limit == 0 or args.limit >= taint_no:
+						print colorama.Fore.LIGHTGREEN_EX + "[+] %d:%d:0x%08x: %s;" % (taint_no, trace.cpu.takt, trace.cpu.eip_before, trace.cpu.instruction) + colorama.Fore.GREEN,
+						for taint_reg in taint_regs:
+							print " %s=0x%08x," % ( taint_reg, trace.cpu.get(taint_reg) ),
+						for taint_mem in taint_mems:
+							print " 0x%08x -> 0x%08x," % ( taint_mem, trace.io.ram.get_dword(taint_mem) ),
+						print colorama.Fore.RESET
+					else:
+						break
 		
-		if args.to_takt and cpu.takt >= args.to_takt:
+		if args.to_takt and trace.cpu.takt >= args.to_takt:
 			break
