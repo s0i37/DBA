9ae87ea19a700d3c1dd65b971bd7b1167f1221a6
s0i37 <s0i37@work>
2019-01-15 20:08:45 +0500

    changes in memtrace

--- memtrace/memtrace.py
+++ memtrace/memtrace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from sys import argv
+from sys import argv, stdout
 import re
 import os
 #import syscalls as SYSCALLS
@@ -14,7 +14,7 @@ WHITESPACE_PADDING_SIZE = 0x50
 pwd = os.path.dirname( os.path.realpath(__file__) )
 trace_file = argv[1]
 
-class Trace:
+class Memtrace:
 	def __init__(self):
 		self.heap = set()
 		self.stack = set()
@@ -44,11 +44,11 @@ class Trace:
 		for page in self.stack:
 			if page <= addr < page | 0xfff:
 				return "stack"
-		return "unknown"
+		return "heap"
 
 
-def load_trace( trace_file ):
-	trace = Trace()
+def get_memtrace( trace_file ):
+	memtrace = Memtrace()
 
 	with open(trace_file) as f:
 		for line in f:
@@ -59,7 +59,7 @@ def load_trace( trace_file ):
 					(_, _, modulename, start, end) = line.split()
 					start = int(start, 16)
 					end = int(end, 16)
-					trace.modules[modulename] = (start,end)
+					memtrace.modules[modulename] = (start,end)
 				elif line.startswith('[*] function'):
 					(_, _, symbol, start, end) = line.split()
 					start = int(start, 16)
@@ -68,7 +68,7 @@ def load_trace( trace_file ):
 				elif line.startswith('[*] allocation'):
 					region = 0
 					size = 0
-					trace.heap.add( (region, region+size) )
+					memtrace.heap.add( (region, region+size) )
 				elif line.find('{') != -1:
 					(eip,opcode,regs) = line.split()
 					takt = int( eip.split(':')[0] )
@@ -76,9 +76,9 @@ def load_trace( trace_file ):
 					opcode = opcode[1:-1]
 					(eax,ecx,edx,ebx,esp,ebp,esi,edi) = map( lambda x: int(x,16), regs.split(',') )
 					try:
-						trace.code[eip].append( [ opcode, (eax,edx,ecx,ebx,esi,edi,ebp,esp), (takt,thread), (), () ] )
+						memtrace.code[eip].append( [ opcode, (eax,ecx,edx,ebx,esp,ebp,esi,edi), (takt,thread), (), () ] )
 					except:
-						trace.code[eip] = [ [ opcode, (eax,edx,ecx,ebx,esi,edi,ebp,esp), (takt,thread), (), () ] ]
+						memtrace.code[eip] = [ [ opcode, (eax,ecx,edx,ebx,esp,ebp,esi,edi), (takt,thread), (), () ] ]
 				elif line.find('[') != -1:
 					(eip,memory,direction,value) = line.split()
 					takt = int( eip.split(':')[0] )
@@ -89,37 +89,37 @@ def load_trace( trace_file ):
 					page >>= 12
 					page <<= 12
 					if direction == "<-":
-						trace.code[eip][-1:][0][4] = (memory,value)
-						try:	trace.memory_writes[memory].append( (eip,value) )
-						except:	trace.memory_writes[memory] = [ (eip,value) ]
-						trace.stack.add(page)
+						memtrace.code[eip][-1:][0][4] = (memory,value)
+						try:	memtrace.memory_writes[memory].append( (eip,value) )
+						except:	memtrace.memory_writes[memory] = [ (eip,value) ]
+						memtrace.stack.add(page)
 					elif direction == "->":
-						trace.code[eip][-1:][0][3] = (memory,value)
-						try:	trace.memory_reads[memory].append( (eip,value) )
-						except:	trace.memory_reads[memory] = [ (eip,value) ]
-						trace.stack.add(page)
+						memtrace.code[eip][-1:][0][3] = (memory,value)
+						try:	memtrace.memory_reads[memory].append( (eip,value) )
+						except:	memtrace.memory_reads[memory] = [ (eip,value) ]
+						memtrace.stack.add(page)
 			except Exception as e:
 				#print str(e)
 				#print line
 				continue
 
-	addresses = trace.memory_writes.keys() + trace.memory_reads.keys()
+	addresses = memtrace.memory_writes.keys() + memtrace.memory_reads.keys()
 	addresses.sort()
 	for addr in addresses:
-		if addr >= max( trace.data_pages ) + WHITESPACE_PADDING_SIZE:
+		if addr >= max( memtrace.data_pages ) + WHITESPACE_PADDING_SIZE:
 			addr //= WHITESPACE_PADDING_SIZE
-			trace.data_pages.append(addr * WHITESPACE_PADDING_SIZE)
-	trace.data_pages.pop(0)
+			memtrace.data_pages.append(addr * WHITESPACE_PADDING_SIZE)
+	memtrace.data_pages.pop(0)
 
-	eips = trace.code.keys()
+	eips = memtrace.code.keys()
 	eips.sort()
 	for eip in eips:
-		if eip >= max( trace.code_pages ) + WHITESPACE_PADDING_SIZE:
+		if eip >= max( memtrace.code_pages ) + WHITESPACE_PADDING_SIZE:
 			eip //= WHITESPACE_PADDING_SIZE
-			trace.code_pages.append(eip * WHITESPACE_PADDING_SIZE)
-	trace.code_pages.pop(0)
+			memtrace.code_pages.append(eip * WHITESPACE_PADDING_SIZE)
+	memtrace.code_pages.pop(0)
 
-	return trace
+	return memtrace
 
 def print_modules(trace):
 	for module,module_range in trace.modules.items():
@@ -146,11 +146,12 @@ def print_syscalls(trace):
 
 
 
-def html_print(trace_file):
-	trace = load_trace( trace_file )
+def create_html(trace_file, outfile='out.html'):
+	out = open(outfile, 'w')
+	memtrace = get_memtrace( trace_file )
 	val = ""
 	operation_count_reads = 0
-	print '''
+	out.write('''
 		<link rel="stylesheet" href="static/jquery-ui/themes/smoothness/jquery-ui.min.css">
 		<link rel="stylesheet" href="static/highlightjs/styles/default.css">
 		<script src="static/jquery/dist/jquery.js"></script>
@@ -167,26 +168,28 @@ def html_print(trace_file):
 		.state, .instructionr, .instructionw, .memoryr, .memoryw { font-size: 9pt; }
 		</style>
 		<div id="dialog"></div>
-	'''
-	print "<div id='data'><table><tbody>"
-	for page in trace.data_pages:
-		page_type = trace.get_page_type(page)
-		print  "<tr><td><a name='data0x%(page)08x'>%(type)s.0x%(page)08x: </a></td>" % { 'page':page, 'type':page_type }
+	''')
+	out.write("<div id='data'><table><tbody>")
+	for page in memtrace.data_pages:
+		page_type = memtrace.get_page_type(page)
+		stdout.write("\r[*] data page 0x%08x" % page)
+		stdout.flush()
+		out.write("<tr><td><a name='data0x%(page)08x'>%(type)s.0x%(page)08x: </a></td>" % { 'page':page, 'type':page_type })
 		addr = page
 		while True:
-			if addr in trace.memory_writes.keys():
+			if addr in memtrace.memory_writes.keys():
 				operation_type = 'w'
-				operation_count_writes = len( trace.memory_writes[addr] )
-				val = "%08X" % trace.memory_writes[addr][0][1]
-				instructions_writes = trace.memory_writes[addr]
-				del trace.memory_writes[addr]
-			if addr in trace.memory_reads.keys():
+				operation_count_writes = len( memtrace.memory_writes[addr] )
+				val = "%08X" % memtrace.memory_writes[addr][0][1]
+				instructions_writes = memtrace.memory_writes[addr]
+				del memtrace.memory_writes[addr]
+			if addr in memtrace.memory_reads.keys():
 				if not val:
 					operation_type = 'r'
-					val = "%08X" % trace.memory_reads[addr][0][1]
-				operation_count_reads = len( trace.memory_reads[addr] )
-				instructions_reads = trace.memory_reads[addr]
-				del trace.memory_reads[addr]
+					val = "%08X" % memtrace.memory_reads[addr][0][1]
+				operation_count_reads = len( memtrace.memory_reads[addr] )
+				instructions_reads = memtrace.memory_reads[addr]
+				del memtrace.memory_reads[addr]
 
 			if val:
 				if operation_type == 'w':
@@ -197,54 +200,58 @@ def html_print(trace_file):
 						reads = ';'.join( map( lambda x: "0x%08x:0x%08X" % (x[0], x[1]) , instructions_reads ) )
 					else:
 						reads = ''
-					heap_number = trace.in_heap(addr)
+					heap_number = memtrace.in_heap(addr)
 					if heap_number:
-						print "<td class='heap%(heap_no)d' style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'heap_no': heap_number%2, 'color':css_color_write, 'r_ops': reads ,'w_ops': writes, 'value': val[-2:] }
+						out.write("<td class='heap%(heap_no)d' style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'heap_no': heap_number%2, 'color':css_color_write, 'r_ops': reads ,'w_ops': writes, 'value': val[-2:] })
 					else:
-						print "<td style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'color': css_color_write, 'r_ops': reads ,'w_ops': writes, 'value': val[-2:] }
+						out.write("<td style='background-color:%(color)s'><div class='byte'>%(value)s</div></td>" % { 'color': css_color_write, 'value': val[-2:] })
+					#reads
+					#writes
 				elif operation_type == 'r':
 					css_color_graient = operation_count_reads * 16 if operation_count_reads < 16 else 255
 					css_color_read = "#%02xff%02x" % (0xff-css_color_graient, 0xff-css_color_graient)
 					reads = ';'.join( map( lambda x: "0x%08x:0x%08X" % (x[0], x[1]) , instructions_reads ) ) or ''
-					heap_number = trace.in_heap(addr)
+					heap_number = memtrace.in_heap(addr)
 					if heap_number:
-						print "<td class='heap%(heap_no)d' style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'heap_no': heap_number%2, 'color':css_color_read, 'r_ops': reads ,'w_ops': '', 'value': val[-2:] }
+						out.write("<td class='heap%(heap_no)d' style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'heap_no': heap_number%2, 'color':css_color_read, 'r_ops': reads ,'w_ops': '', 'value': val[-2:] })
 					else:
-						print "<td style='background-color:%(color)s'><div class='byte' r='%(r_ops)s' w='%(w_ops)s'>%(value)s</div></td>" % { 'color': css_color_read, 'r_ops': reads ,'w_ops': '', 'value': val[-2:] }
-						
+						out.write("<td style='background-color:%(color)s'><div class='byte'>%(value)s</div></td>" % { 'color': css_color_read, 'value': val[-2:] })
+					#reads
 				val = val[:-2]
 			else:
-				heap_number = trace.in_heap(addr)
+				heap_number = memtrace.in_heap(addr)
 				if heap_number:
-					print "<td class='heap%d'>**</td>" % (heap_number%2)
+					out.write("<td class='heap%d'>**</td>" % (heap_number%2))
 				else:
-					print "<td>**</td>"
+					out.write("<td>**</td>")
 
 			addr += 1
 			if addr >= page + WHITESPACE_PADDING_SIZE:
 				break
 			if not addr % 0x10:
-				print "</tr>"
-				print "<tr><td><a name='data0x%(addr)08x'>%(type)s.0x%(addr)08x: </a></td>" % { 'addr': addr, 'type': page_type }
-		print "</tr>"
-	print "</tbody></table></div>"
+				out.write("</tr>")
+				out.write("<tr><td><a name='data0x%(addr)08x'>%(type)s.0x%(addr)08x: </a></td>" % { 'addr': addr, 'type': page_type })
+		out.write("</tr>")
+	out.write("</tbody></table></div>")
 
-	print "<div id='code'><table><tbody>"
+	out.write("<div id='code'><table><tbody>")
 	dis = capstone.Cs( capstone.CS_ARCH_X86, capstone.CS_MODE_32 )
-	for page in trace.code_pages:
+	for page in memtrace.code_pages:
 		eip = page
 		while True:
-			if eip in trace.code.keys():
+			if eip in memtrace.code.keys():
+				stdout.write("\r[*] code 0x%08x" % eip)
+				stdout.flush()
 				states = []
 				r_ops = []
 				w_ops = []
 				rw_addr = 0
 				executions = 0
-				for opcode, regs, opts, mem_r, mem_w in trace.code[eip]:
+				for opcode, regs, opts, mem_r, mem_w in memtrace.code[eip]:
 					executions += 1
-					eax,edx,ecx,ebx,esi,edi,ebp,esp = regs
+					eax,ecx,edx,ebx,esp,ebp,esi,edi = regs
 					number, threadid = opts
-					states.append( "%s:%s:%s,%s,%s,%s,%s,%s,%s,%s" % (number,threadid, eax,edx,ecx,ebx,esi,edi,ebp,esp) )
+					states.append( "%s:%s:%s,%s,%s,%s,%s,%s,%s,%s" % (number,threadid, eax,ecx,edx,ebx,esp,ebp,esi,edi) )
 					if mem_r:
 						rw_addr,rw_val = mem_r
 						r_ops.append( "0x%08x:0x%08X" % (rw_addr, rw_val) )
@@ -261,31 +268,36 @@ def html_print(trace_file):
 						break
 				css_color_gradient = executions * 10 if executions * 10 < 255 else 255
 				css_color_executions = "#ff%02x%02x" % (0xff-css_color_gradient, 0xff-css_color_gradient)
+				#r_ops
+				#w_ops
+				#states
 				if rw_addr:
-					print '<tr style="background-color:%(color)s"><td class="states" states="%(states)s">%(exec_count)d</td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>%(opcode)s</td><td><a href="#"><code class="instr" r="%(r_ops)s" w="%(w_ops)s">%(instr)s</code></a></td></tr>' % { 'color': css_color_executions, 'exec_count': executions, 'addr': eip, 'opcode': opcode, 'instr': disasm, 'states': ';'.join(states), 'r_ops': ';'.join(r_ops), 'w_ops': ';'.join(w_ops) }
+					out.write('<tr style="background-color:%(color)s"><td class="states">%(exec_count)d</td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>%(opcode)s</td><td><a href="#"><code class="instr">%(instr)s</code></a></td></tr>' % { 'color': css_color_executions, 'exec_count': executions, 'addr': eip, 'opcode': opcode, 'instr': disasm })
 				else:
-					print '<tr style="background-color:%(color)s"><td class="states" states="%(states)s">%(exec_count)d</td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>%(opcode)s</td><td><code class="instr">%(instr)s</code></td></tr>' % { 'color': css_color_executions, 'exec_count': executions, 'addr':eip, 'opcode': opcode, 'instr': disasm, 'states': ';'.join(states) }
+					out.write('<tr style="background-color:%(color)s"><td class="states">%(exec_count)d</td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>%(opcode)s</td><td><code class="instr">%(instr)s</code></td></tr>' % { 'color': css_color_executions, 'exec_count': executions, 'addr':eip, 'opcode': opcode, 'instr': disasm })
 				eip += next_instruction_offset
 			else:
-				print '<tr><td></td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>**</td><td>**</td></tr>' % { 'addr': eip }
+				out.write('<tr><td></td><td><a name="code0x%(addr)08x">0x%(addr)08x</a></td><td>**</td><td>**</td></tr>' % { 'addr': eip })
 				eip += 1
 			if eip >= page + WHITESPACE_PADDING_SIZE:
 				break
-	print "</tbody></table></div>"
+	out.write("</tbody></table></div>")
+	o.close()
 
-print "<pre>"
+#print "<pre>"
 #print_modules()
 #print_memory_pages()
 #print_stack_pages()
 #print_heap_regions()
 #print_syscalls()
-print "</pre>"
-html_print( trace_file )
+#print "</pre>"
+create_html( trace_file )
 
 '''
 TODO:
 сделать поддержку экспорта символов
-значения памяти и адреса инструкций записывать в базу - т.к. даже для /bin/ls файл получается слишком большой.
-нужен маленький веб-сервер, который будет извлекать значения памяти и адреса инструкций из базы веб-странице.
-+сервер можно реализовать прямо в memtrace.py
+*значения памяти и адреса инструкций записывать в базу - т.к. даже для /bin/ls файл получается слишком большой.
+*нужен маленький веб-сервер, который будет извлекать значения памяти и адреса инструкций из базы веб-странице.
+*сервер можно реализовать прямо в memtrace.py
+!дизасм идёт по одному байту
 '''
