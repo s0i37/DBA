582bbf6a41772d92bfb4945e67ff0a4780781944
s0i37 <s0i37@work>
2019-01-16 20:14:05 +0500

    added many changes in disas

--- disas.py
+++ disas.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from lib.emulate import Trace, CPU
+from lib.emulate import Trace
 from sys import stdin
 import argparse
 import colorama
@@ -8,17 +8,19 @@ import colorama
 parser = argparse.ArgumentParser( description='data flow analisys tool' )
 parser.add_argument("tracefile", type=str, help="trace.txt")
 
-parser.add_argument("-from_takt", type=int, default=0, help="print tainted instruction only after takt")
-parser.add_argument("-to_takt", type=int, default=0, help="print tainted instruction only before takt")
-parser.add_argument("-takts", type=int, default=0, help="print tainted instruction only before takt")
+parser.add_argument("-from_takt", type=int, default=0, help="disas only after takt")
+parser.add_argument("-to_takt", type=int, default=0, help="disas only before takt")
+parser.add_argument("-takts", type=int, default=0, help="disas only N takts")
 
 parser.add_argument("-regs", type=str, default='', help="print reg1,reg2,regN")
+parser.add_argument("-mems", type=str, default='', help="print [imm1],[reg1],[immN]")
 
-parser.add_argument("-deep", type=int, default=-1, help="print tainted instruction only before takt")
-parser.add_argument("-func", dest="just_func", type=bool, default=False, help="disas just function")
+parser.add_argument("-function", dest="just_function", type=bool, default=False, help="disas only current function")
+parser.add_argument("-from_deep", type=int, default=-1, help="disas only deeper then N")
+parser.add_argument("-to_deep", type=int, default=-1, help="disas only not deeper then N")
 
-parser.add_argument("-ir", dest="ir", type=bool, default=False, help="IR")
-parser.add_argument("-anal", dest="anal", type=bool, default=False, help="analyze")
+parser.add_argument("-ir", dest="ir", type=bool, default=False, help="print IR instead of disas")
+parser.add_argument("-anal", dest="anal", type=bool, default=False, help="print REGs access, MEMs access")
 
 parser.add_argument("-diff", type=str, default='', help="print difference between two traces")
 args = parser.parse_args()
@@ -61,6 +63,8 @@ if args.diff:
 
 with Trace( open(args.tracefile) if args.tracefile != '-' else stdin ) as trace:
 	deep = 0
+	after_call = False
+	after_ret = False
 	while True:
 		used_registers, used_memory = None, None
 		if args.anal:
@@ -70,23 +74,39 @@ with Trace( open(args.tracefile) if args.tracefile != '-' else stdin ) as trace:
 		else:
 			info = trace.instruction()
 
+		if args.from_takt and trace.cpu.takt < args.from_takt:
+			continue
+		if args.to_takt and trace.cpu.takt > args.to_takt:
+			break
+
 		args.takts -= 1
 		if args.takts == 0:
 			break
 
 		mnem = trace.cpu.disas()
 
-		if args.deep >= 0 and deep > args.deep:
-			continue
+		if after_call:
+			deep += 1
+			after_call = False
+		elif after_ret:
+			deep -= 1
+			after_ret = False
 
 		if mnem.split()[0] in ('call', ):
-			deep += 1
+			after_call = True
 		elif mnem.split()[0] in ('ret', ):
-			deep -= 1
+			after_ret = True
 
-		if args.just_func and mnem[0] in ('ret', ):
-			break
+		if args.to_deep >= 0 and deep > args.to_deep:
+			continue
+		if args.from_deep >= 0 and deep < args.from_deep:
+			continue
 
+		if args.just_function:
+			if deep > 0:
+				continue
+			if deep < 0:
+				break
 
 		if args.diff:
 			if not new_eips:
@@ -109,24 +129,35 @@ with Trace( open(args.tracefile) if args.tracefile != '-' else stdin ) as trace:
 				print irblock.to_string(loc_db)
 		else:
 			print "{takt}:{offset}: {disas}".format( takt=trace.cpu.takt, offset=hex(trace.cpu.eip_before), disas=mnem ),
-			if args.regs:
-				print ';',
-				for reg in args.regs.split(','):
-					print "%s=0x%X," % (reg, trace.cpu.get(reg)),
-			print ''
 		
+		if args.regs:
+			print ';',
+			for reg in args.regs.split(','):
+				print "%s=0x%X," % (reg, trace.cpu.get(reg)),
+		if args.mems:
+			print ';',
+			for mem in args.mems.split(','):
+				try:
+					imm = int(mem,16)
+					print "[0x%08x]=0x%08X," % (imm, trace.io.ram.get_dword(imm)),
+				except:
+					reg = mem
+					ptr = trace.cpu.get(reg)
+					print "[%s=0x%08x]=0x%08X," % (reg, ptr, trace.io.ram.get_dword(ptr)),
 
 		if used_registers:
+			print ';',
 			regs_r, regs_w = used_registers
 			for reg_r in regs_r:
 				print "%s -> 0x%X," % (reg_r, trace.cpu.get(reg_r, when='before')),
 			for reg_w in regs_w:
 				print "%s <- 0x%X," % (reg_w, trace.cpu.get(reg_w, when='after')),
-			print ''
 		if used_memory:
+			print ';',
 			mems_r, mems_w = used_memory
 			for mem_r in mems_r:
 				print "[0x%x] -> 0x%X," % (mem_r, trace.io.ram.get_dword(mem_r)),
 			for mem_w in mems_w:
 				print "[0x%x] <- 0x%X," % (mem_w, trace.io.ram.get_dword(mem_w)),
-			print ''
+
+		print ''
