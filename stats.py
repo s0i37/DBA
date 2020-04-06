04bd2c4bc32136e64e3b025a79e8adf3fa035dc5
soier <soier@work>
2020-04-07 02:16:48 +0500

    improved stats

--- stats.py
+++ stats.py
@@ -1,96 +1,115 @@
 #!/usr/bin/python
 from lib.emulate import Trace, StopExecution
+from os import path
 import argparse
 import colorama
 
 
-parser = argparse.ArgumentParser( description='data flow analisys tool' )
+parser = argparse.ArgumentParser( description='show executed statistic' )
 parser.add_argument("tracefile", type=str, help="trace.txt")
+parser.add_argument("-modules", action='store_true', default=False, help="show modules statistic")
+parser.add_argument("-module", type=str, default='', help="show statistic only by module")
+parser.add_argument("-symbols", action='store_true', default=False, help="show symbols statistic")
+
 parser.add_argument("-diff", type=str, default='', help="print difference between two traces")
 parser.add_argument("-comp", type=str, default='', help="print comparison between two traces")
 args = parser.parse_args()
 
-modules_used = {}
-symbols_used = {}
-modules_exec = {}
-symbols_exec = {}
 
-modules = {}
-symbols = {}
+def get_module(modules, addr):
+	for module_name,module_range in modules.items():
+		(start,end) = module_range
+		if start <= addr <= end:
+			return path.basename(module_name.replace('\\','/')), (start, end)
+	return None, (0,0)
 
-#for performance reason
-last_module = {}
-last_symbol = {}
+def get_symbol(symbols, addr):
+	for symbol_name,symbol_range in symbols.items():
+		(start,end) = symbol_range
+		if addr == start:
+			return symbol_name
 
-def get_module(eip):
-	global modules
-	for (modulename,_range) in modules.items():
-		(start,end) = _range
-		if start <= eip <= end:
-			return { 'name': modulename, 'start': start, 'end': end }
-	return {}
+class Module:
+	def __init__(self, addr):
+		self.addr = addr
+		self.execs = 0
 
-def get_symbol(eip):
-	global symbols
-	for (symbol,_range) in symbols.items():
-		(start,end) = _range
-		if start <= eip <= end:
-			return { 'name': symbol, 'start': start, 'end': end }
-	return {}
-
-def whereis(eip):
-	global last_module, last_symbol, modules_used, symbols_used
-	if not ( last_module and last_module['start'] <= eip <= last_module['end'] ):
-		last_module = get_module(eip)
-
-		if last_module and not last_module['name'] in modules_used.keys():
-			modules_used[ last_module['name'] ] = [ last_module['start'], last_module['end'] ]
-
-	if not last_symbol or last_symbol['start'] > eip or eip > last_symbol['end']:
-		last_symbol = get_symbol(eip)
-		if last_symbol and not last_symbol['name'] in symbols_used.keys():
-			symbols_used[ last_symbol['name'] ] = [ last_symbol['start'], last_symbol['end'] ]
-	return (last_module,last_symbol)
-
-
-instr = 0
-memop_r = 0
-memop_w = 0
+	def load_module(self, modules):
+		(self.name, _range) = get_module(modules, self.addr)
+		(self.start, self.end) = _range 
 
 class Function:
-	def __init__(self, start):
-		self.start = start
+	def __init__(self, addr):
+		self.addr = addr
 		self.execs = 0
 		self.calls = 0
 
-def get_covered_functions(tracefile):
+	def load_symbol(self, symbols):
+		self.symbol = get_symbol(symbols, self.addr)
+
+	def get_module(self, modules):
+		module = Module(self.addr)
+		module.load_module(modules)
+		if module.end != 0:
+			return module
+
+def find_module(modules, addr):
+	for module in modules:
+		if module.start <= addr <= module.end:
+			return module
+
+def in_module(addr, module_name):
+	global modules
+	for module in modules:
+		if module.start <= addr <= module.end and module.name.lower() == module_name:
+			return True
+	return False
+
+def get_coverage(tracefile):
 	current_function = None
+	modules = []
 	functions = {}
-	stack = []
+	stack = {}
 	with Trace( open(tracefile) ) as trace:
 		try:
 			while True:
 				trace.instruction()
+				thr = trace.cpu.thread_id
 				if not current_function:
 					current_function = functions.get(trace.cpu.eip_before)
 					if current_function:
 						current_function.calls += 1
 					else:
 						current_function = Function(trace.cpu.eip_before)
+						current_function.load_symbol(trace.symbols)
 						current_function.calls += 1
 						functions[trace.cpu.eip_before] = current_function
+
+					current_module = find_module(modules, trace.cpu.eip_before)
+					if not current_module:
+						current_module = current_function.get_module(trace.modules)
+						if current_module:
+							modules.append(current_module)
+					
+				if current_module:
+					current_module.execs += 1
 				current_function.execs += 1
 
 				mnem = trace.cpu.disas()
+				if not thr in stack.keys():
+					stack[thr] = []
 				if mnem.split()[0] in ('call', ):
-					stack.append(current_function)
+					stack[thr].append(current_function)
 					current_function = None
+					current_module = None
 				elif mnem.split()[0] in ('ret', ):
-					try:	current_function = stack.pop()
+					try:	current_function = stack[thr].pop()
 					except:	current_function = Function(0)
-
+		
+		except KeyboardInterrupt:
+			return (modules,functions)
 		except StopExecution:
-			return functions
+			return (modules,functions)
 
 def diff_cover(functions_after, functions_before):
 	functions_diff = {}
@@ -99,18 +118,33 @@ def diff_cover(functions_after, functions_before):
 			functions_diff[addr] = functions_after[addr]
 	return functions_diff
 
+def print_modules_stats(modules):
+	stats = []
+	for module in modules:
+		stats.append( ( module.start, module.name, module.execs ) )
 
-def print_stats(functions):
+	print ""
+	print "base: \t\t module: \t execs:"
+	for stat in sorted(stats, key=lambda columns: columns[0], reverse=False):
+		print "{base} \t {module} \t {execs}".format(base=hex(stat[0]), module=stat[1], execs=stat[2])
+
+def print_functions_stats(functions):
 	stats = []
 	for addr,function in functions.items():
-		stats.append( ( addr, function.calls, function.execs ) )
+		if not args.module or in_module(addr, args.module):
+			if function.symbol:
+				addr = function.symbol
+			else:
+				addr = "0x%08x" % addr
+			stats.append( ( addr, function.calls, function.execs ) )
 
-	print "\nsymbol:\t\tcalls:\texecs:"
+	print ""
+	print "symbol: \t calls:  execs:"
 	for stat in sorted(stats, key=lambda columns: columns[1], reverse=True):
-		print "0x%08x\t%d\t%d" % ( stat[0], stat[1], stat[2] )
+		print "{function} \t {calls} \t {execs}".format( function=stat[0], calls=stat[1], execs=stat[2] )
 
 
-def print_compared_stats(functions_after, functions_before):
+def print_compared_functions_stats(functions_after, functions_before):
 	def print_diff(a,b):
 		if a > b:
 			print "+%d\t" % (a-b),
@@ -148,87 +182,16 @@ def print_compared_stats(functions_after, functions_before):
 			print colorama.Fore.RED + "0x%08x\t-\t-\t%d\t%d" % ( stat[0], stat[1], stat[2] ) + colorama.Fore.RESET
 
 
-functions = get_covered_functions(args.tracefile)
+(modules,functions) = get_coverage(args.tracefile)
 if args.diff:
-	functions_diff = get_covered_functions(args.diff)
+	(modules_diff,functions_diff) = get_coverage(args.diff)
 	functions = diff_cover(functions, functions_diff)
 elif args.comp:
-	functions_comp = get_covered_functions(args.comp)
-	print_compared_stats(functions, functions_comp)
+	(modules_comp,functions_comp) = get_coverage(args.comp)
+	print_compared_functions_stats(functions, functions_comp)
 	exit()
-print_stats(functions)
 
-
-exit()
-#load trace
-if os.path.isfile(trace_file):
-	with open(trace_file) as f:
-		for line in f:
-			try:
-				#comment found
-				if line.startswith('[#]'):
-					continue
-				#module found
-				elif line.startswith('[*] module'):
-					(_, _, modulename, start, end) = line.split()
-					start = int(start, 16)
-					end = int(end, 16)
-					modules[modulename] = [start, end]
-					print "[*] %s 0x%08x 0x%08x" % (modulename, start, end)
-					continue
-				#symbol found
-				elif line.startswith('[*] function'):
-					(_, _, symbol, start, end) = line.split()
-					start = int(start, 16)
-					end = int(end, 16)
-					symbols[symbol] = [start, end]
-					continue
-				#instruction found
-				elif line.find('{') != -1:
-					(eip,opcode,regs) = line.split()
-					takt = int( eip.split(':')[0] )
-					(eip,thread) = map( lambda x: int(x, 16), eip.split(':')[1:] )
-					(eax,ecx,edx,ebx,esp,ebp,esi,edi) = map( lambda x: int(x,16), regs.split(',') )
-					memory = None
-				#memory found
-				elif line.find('[') != -1:
-					(eip,memory,direction,value) = line.split()
-					takt = int( eip.split(':')[0] )
-					(eip,thread) = map( lambda x: int(x, 16), eip.split(':')[1:] )
-					memory = int( memory[1:-1], 16 )
-					value = int( value, 16 )
-					opcode = None
-					if direction == "->":
-						memop_r += 1
-					elif direction == "<-":
-						memop_w += 1
-				#nothing
-				else:
-					continue
-			except Exception as e:
-				continue
-
-			if opcode:
-				(module,symbol) = whereis(eip)
-				if module:
-					try:
-						modules_exec[ module['name'] ] += 1
-					except:
-						modules_exec[ module['name'] ] = 1
-				if symbol:
-					try:
-						symbols_exec[ symbol['name'] ] += 1
-					except:
-						symbols_exec[ symbol['name'] ] = 1
-
-			if takt and takt % 10000 == 0:
-				stdout.write( "\rx:%d, r:%d, w:%d %s %s" % ( takt, memop_r, memop_w, last_module.get('name') or '', last_symbol.get('name') or '' ) )
-				stdout.flush()
-
-print ''
-for module,module_range in modules_used.items():
-	print "%s\t\t%d execs" % ( module, modules_exec.get(module, 0) )
-	for symbol,symbol_range in symbols_used.items():
-		if module_range[0] <= symbol_range[0] and symbol_range[1] <= module_range[1]:
-			print "\t%s\t%d execs" % ( symbol, symbols_exec.get(symbol, 0) )
-
+if args.modules:
+	print_modules_stats(modules)
+if args.symbols:
+	print_functions_stats(functions)
