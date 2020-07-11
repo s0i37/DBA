901b04e085454aa446c94d46799a86278eba3f63
soier <soier@work>
2020-07-11 14:37:20 +0500

    triton taint/concolic scripts for trace analisys

--- triton/concolic.py
+++ triton/concolic.py
@@ -1,88 +1,166 @@
 #!/usr/bin/python3.8
 from triton import *
+import argparse
+from struct import pack
+from os import path
+import codecs
 
-code = {
-	0x555555555145:      b"\x55",
-	0x555555555146:      b"\x48\x89\xe5",
-	0x555555555149:      b"\x48\x89\x7d\xe8",
-	0x55555555514d:      b"\x48\x8b\x45\xe8",
-	0x555555555151:      b"\x48\x83\xc0\x03",
-	0x555555555155:      b"\x0f\xb6\x00",
-	0x555555555158:      b"\x88\x45\xff",
-	0x55555555515b:      b"\x48\x8b\x45\xe8",
-	0x55555555515f:      b"\x0f\xb7\x00",
-	0x555555555162:      b"\x66\x89\x45\xfc",
-	0x555555555166:      b"\x80\x7d\xff\x00",
-	0x55555555516a:      b"\x74\x33",
-	0x55555555516c:      b"\x80\x7d\xff\x72",
-	0x555555555170:      b"\x75\x26",
-	0x555555555172:      b"\x0f\xb6\x45\xff",
-	0x555555555176:      b"\x83\xc0\x22",
-	0x555555555179:      b"\x88\x45\xfb",
-	0x55555555517c:      b"\x80\x75\xfb\x11",
-	0x555555555180:      b"\x0f\xb6\x45\xfb",
-	0x555555555184:      b"\x66\x39\x45\xfc",
-	0x555555555188:      b"\x75\x07",
-	0x55555555518a:      b"\xb8\x03\x00\x00\x00",
-	0x55555555518f:      b"\xeb\x13",
-	0x555555555191:      b"\xb8\x02\x00\x00\x00",
-	0x555555555196:      b"\xeb\x0c",
-	0x555555555198:      b"\xb8\x01\x00\x00\x00",
-	0x55555555519d:      b"\xeb\x05",
-	0x55555555519f:      b"\xb8\x00\x00\x00\x00",
-	0x5555555551a4:      b"\x5d",
-	0x5555555551a5:      b"\xc3",
-}
 
 ctx = TritonContext()
 ctx.setArchitecture(ARCH.X86_64)
 ctx.setMode(MODE.ALIGNED_MEMORY, True)
+astCtxt = ctx.getAstContext()
 #ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
 
-ctx.setConcreteMemoryAreaValue(0x7ffffffde000, open('/tmp/1.bin','rb').read())
-ctx.setConcreteMemoryAreaValue(0x555555555000, open('/tmp/2.bin','rb').read())
-ctx.setConcreteRegisterValue(ctx.registers.rax, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.rbx, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.rcx, 0x7ffff7fa2a00)
-ctx.setConcreteRegisterValue(ctx.registers.rdx, 0x7ffff7fa5590)
-ctx.setConcreteRegisterValue(ctx.registers.rsi, 0x74726577)
-ctx.setConcreteRegisterValue(ctx.registers.rdi, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.r8, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.r9, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.r10, 0x00000410)
-ctx.setConcreteRegisterValue(ctx.registers.r11, 0x00000246)
-ctx.setConcreteRegisterValue(ctx.registers.r12, 0x555555555060)
-ctx.setConcreteRegisterValue(ctx.registers.r13, 0x7fffffffe230)
-ctx.setConcreteRegisterValue(ctx.registers.r14, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.r15, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.rip, 0x55555555514d)
-ctx.setConcreteRegisterValue(ctx.registers.rbp, 0x7fffffffe130)
-ctx.setConcreteRegisterValue(ctx.registers.eflags, 0x00000246)
-ctx.setConcreteRegisterValue(ctx.registers.rsp, 0x7fffffffe130)
+def emulate(instruction):
+	for addr in instruction.mem:
+		if not ctx.isMemorySymbolized(addr):
+			ctx.setConcreteMemoryAreaValue(addr, instruction.mem[addr])
 
-ctx.symbolizeMemory(MemoryAccess(0x7fffffffe146, 8))
-addr = 0x55555555514d
-while addr in code:
+	if not ctx.isRegisterSymbolized(ctx.registers.rax):
+		ctx.setConcreteRegisterValue(ctx.registers.rax, instruction.rax)
+	if not ctx.isRegisterSymbolized(ctx.registers.rbx):
+		ctx.setConcreteRegisterValue(ctx.registers.rbx, instruction.rbx)
+	if not ctx.isRegisterSymbolized(ctx.registers.rcx):
+		ctx.setConcreteRegisterValue(ctx.registers.rcx, instruction.rcx)
+	if not ctx.isRegisterSymbolized(ctx.registers.rdx):
+		ctx.setConcreteRegisterValue(ctx.registers.rdx, instruction.rdx)
+	if not ctx.isRegisterSymbolized(ctx.registers.rsi):
+		ctx.setConcreteRegisterValue(ctx.registers.rsi, instruction.rsi)
+	if not ctx.isRegisterSymbolized(ctx.registers.rdi):
+		ctx.setConcreteRegisterValue(ctx.registers.rdi, instruction.rdi)
+	if not ctx.isRegisterSymbolized(ctx.registers.rbp):
+		ctx.setConcreteRegisterValue(ctx.registers.rbp, instruction.rbp)
+	if not ctx.isRegisterSymbolized(ctx.registers.rsp):
+		ctx.setConcreteRegisterValue(ctx.registers.rsp, instruction.rsp)
+
 	inst = Instruction()
-	inst.setOpcode(code[addr])
-	inst.setAddress(addr)
+	inst.setOpcode(instruction.opcode)
+	inst.setAddress(instruction.addr)
 	ctx.processing(inst)
 	if inst.isSymbolized():
 		print('[symbolic] %s' % str(inst))
-		#print("  reg read:" + str(inst.getReadRegisters()))
-		#print("  reg write:" + str(inst.getWrittenRegisters()))
-		#print("  mem read:" + str(inst.getLoadAccess()))
-		#print("  mem write:" + str(inst.getStoreAccess()))
-		#for expression in inst.getSymbolicExpressions():
-		#	print("\t" + str(expression.getAst()))
 		if inst.isBranch():
-			#if ctx.getPathConstraints()[0].isMultipleBranches():
-			for constraint in ctx.getModel(ctx.getPathPredicate()).values():
+			#for constraint in ctx.getModel(ctx.getPathPredicate()).values():
+			for constraint in ctx.getModel(astCtxt.lnot(ctx.getPathPredicate())).values():
+				#import ipdb;ipdb.set_trace()
 				name = constraint.getVariable().getName()
 				size = int(constraint.getVariable().getBitSize()/8)
 				solve = constraint.getValue().to_bytes(size, byteorder='little')
 				print("[+] {addr}: {solve}".format(addr=hex(inst.getAddress()), solve=str(solve)))
-			#ctx.getPathConstraints()[0].getBranchConstraints()
-	else:
-		print('[*] %s' % str(inst))
-	addr = ctx.getConcreteRegisterValue(ctx.registers.rip)
+
+memory = {}
+def save_mem(addr, mem):
+	global memory
+	for i in range(len(mem)):
+		memory[addr+i] = mem[i]
+
+def find_mem(addr, needle, size):
+	was_found = False
+	high_boundary_search = addr + size
+	while addr <= high_boundary_search:
+		for i in range(len(needle)):
+			value = memory.get(addr + i)
+			if value == None:
+				return None
+			if needle.find( chr(value) ) == -1:
+				was_found = False
+				addr += 1
+				break
+			else:
+				was_found = True
+		if was_found:
+			return addr
+	return None
+
+def align(size):
+	return ((size >> 2) +1) << 2
+
+class Step:
+	def __init__(self):
+		self.mem = {}
+	def mem_read(self, addr, val):
+		self.mem[addr] = val
+	def mem_write(self, addr, val):
+		self.mem[addr] = val
+	def set_registers(self, regs):
+		(self.rax,self.rcx,self.rdx,self.rbx,self.rsp,self.rbp,self.rsi,self.rdi) = regs
+
+
+parser = argparse.ArgumentParser( description='data flow analisys tool' )
+parser.add_argument("tracefile", type=str, help="trace.log")
+parser.add_argument("-symbolic_data", type=str, default='', help='symbolic data: "GET / HTTP/1.1" or input.bin')
+args = parser.parse_args()
+
+if path.isfile(args.symbolic_data):
+	with open(args.symbolic_data) as f:
+		args.symbolic_data = f.read()
+
+instruction = None
+has_symbolic_data = False
+symbolic_memory = set()
+with open(args.tracefile, 'r') as f:
+	while True:
+		line = f.readline()
+		if not line:
+			break
+		if line.startswith('['):
+			continue
+
+		line = line.split('\n')[0]
+		#print(line)
+		if line.find('{') != -1:
+			addr,opcode,regs = line.split(' ')
+			opcode = codecs.decode(opcode[1:-1],'hex')
+			rax,rcx,rdx,rbx,rsp,rbp,rsi,rdi = map(lambda r:int(r,16), regs.split(','))
+			if instruction and has_symbolic_data:
+				try:
+					emulate(instruction)
+				except Exception as e:
+					#print(hex(instruction.addr))
+					#print(str(e))
+					pass
+			instruction = Step()
+			instruction.set_registers((rax,rcx,rdx,rbx,rsp,rbp,rsi,rdi))
+			instruction.opcode = opcode
+			icount,addr,thread = addr.split(':')
+			icount = int(icount)
+			if icount % 10000 == 0:
+				print("[*] %d" % icount)
+			instruction.addr = int(addr, 16)
+		elif line.find('[') != -1:
+			if line.find('-') != -1:
+				addr,mem,access,val = line.split(' ')
+				mem = int(mem[1:-1], 16)
+				size = len(val[2:])
+				if not val:
+					continue
+				val = int(val, 16)
+				if size == 2: 
+					val = pack('B', val)
+				elif size == 4:
+					val = pack('<H', val)
+				elif size == 8:
+					val = pack('<L', val)
+				elif size == 16:
+					val = pack('<Q', val)
+				else:
+					val = None
+				if val:
+					if access == '->':
+						instruction.mem_read(mem, val)
+					elif access == '<-':
+						instruction.mem_write(mem, val)
+			elif line.find(':') != -1:
+				addr,mem,val = line.split(' ')
+				mem = int(mem[1:-2], 16)
+				val = codecs.decode(val,'hex')
+				save_mem(mem,val)
+				ptr = find_mem(mem, args.symbolic_data, len(val))
+				if ptr and not ptr in symbolic_memory:
+					print("[*] symbolize memory 0x%08x" % ptr)
+					#import ipdb;ipdb.set_trace()
+					ctx.setConcreteMemoryAreaValue(ptr, bytes(args.symbolic_data, 'utf8'))
+					ctx.symbolizeMemory(MemoryAccess(ptr, align(len(args.symbolic_data))))
+					has_symbolic_data = True
+					symbolic_memory.add(ptr)
--- triton/taint.py
+++ triton/taint.py
@@ -1,74 +1,156 @@
 #!/usr/bin/python3.8
 from triton import *
-
-code = {
-	0x555555555145:      b"\x55",
-	0x555555555146:      b"\x48\x89\xe5",
-	0x555555555149:      b"\x48\x89\x7d\xe8",
-	0x55555555514d:      b"\x48\x8b\x45\xe8",
-	0x555555555151:      b"\x48\x83\xc0\x03",
-	0x555555555155:      b"\x0f\xb6\x00",
-	0x555555555158:      b"\x88\x45\xff",
-	0x55555555515b:      b"\x48\x8b\x45\xe8",
-	0x55555555515f:      b"\x0f\xb7\x00",
-	0x555555555162:      b"\x66\x89\x45\xfc",
-	0x555555555166:      b"\x80\x7d\xff\x00",
-	0x55555555516a:      b"\x74\x33",
-	0x55555555516c:      b"\x80\x7d\xff\x72",
-	0x555555555170:      b"\x75\x26",
-	0x555555555172:      b"\x0f\xb6\x45\xff",
-	0x555555555176:      b"\x83\xc0\x22",
-	0x555555555179:      b"\x88\x45\xfb",
-	0x55555555517c:      b"\x80\x75\xfb\x11",
-	0x555555555180:      b"\x0f\xb6\x45\xfb",
-	0x555555555184:      b"\x66\x39\x45\xfc",
-	0x555555555188:      b"\x75\x07",
-	0x55555555518a:      b"\xb8\x03\x00\x00\x00",
-	0x55555555518f:      b"\xeb\x13",
-	0x555555555191:      b"\xb8\x02\x00\x00\x00",
-	0x555555555196:      b"\xeb\x0c",
-	0x555555555198:      b"\xb8\x01\x00\x00\x00",
-	0x55555555519d:      b"\xeb\x05",
-	0x55555555519f:      b"\xb8\x00\x00\x00\x00",
-	0x5555555551a4:      b"\x5d",
-	0x5555555551a5:      b"\xc3",
-}
+import argparse
+from struct import pack
+from os import path
+import codecs
 
 ctx = TritonContext()
 ctx.setArchitecture(ARCH.X86_64)
 ctx.setMode(MODE.ALIGNED_MEMORY, True)
 #ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
 
-ctx.setConcreteMemoryAreaValue(0x7ffffffde000, open('/tmp/1.bin','rb').read())
-ctx.setConcreteMemoryAreaValue(0x555555555000, open('/tmp/2.bin','rb').read())
-ctx.setConcreteRegisterValue(ctx.registers.rax, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.rbx, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.rcx, 0x7ffff7fa2a00)
-ctx.setConcreteRegisterValue(ctx.registers.rdx, 0x7ffff7fa5590)
-ctx.setConcreteRegisterValue(ctx.registers.rsi, 0x74726577)
-ctx.setConcreteRegisterValue(ctx.registers.rdi, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.r8, 0x7fffffffe146)
-ctx.setConcreteRegisterValue(ctx.registers.r9, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.r10, 0x00000410)
-ctx.setConcreteRegisterValue(ctx.registers.r11, 0x00000246)
-ctx.setConcreteRegisterValue(ctx.registers.r12, 0x555555555060)
-ctx.setConcreteRegisterValue(ctx.registers.r13, 0x7fffffffe230)
-ctx.setConcreteRegisterValue(ctx.registers.r14, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.r15, 0x00000000)
-ctx.setConcreteRegisterValue(ctx.registers.rip, 0x55555555514d)
-ctx.setConcreteRegisterValue(ctx.registers.rbp, 0x7fffffffe130)
-ctx.setConcreteRegisterValue(ctx.registers.eflags, 0x00000246)
-ctx.setConcreteRegisterValue(ctx.registers.rsp, 0x7fffffffe130)
+def emulate(instruction):
+	for addr in instruction.mem:
+		ctx.setConcreteMemoryAreaValue(addr, instruction.mem[addr])
+	
+	ctx.setConcreteRegisterValue(ctx.registers.rax, instruction.rax)
+	ctx.setConcreteRegisterValue(ctx.registers.rbx, instruction.rbx)
+	ctx.setConcreteRegisterValue(ctx.registers.rcx, instruction.rcx)
+	ctx.setConcreteRegisterValue(ctx.registers.rdx, instruction.rdx)
+	ctx.setConcreteRegisterValue(ctx.registers.rsi, instruction.rsi)
+	ctx.setConcreteRegisterValue(ctx.registers.rdi, instruction.rdi)
+	ctx.setConcreteRegisterValue(ctx.registers.rip, instruction.addr)
+	ctx.setConcreteRegisterValue(ctx.registers.rbp, instruction.rbp)
+	ctx.setConcreteRegisterValue(ctx.registers.rsp, instruction.rsp)
 
-ctx.taintMemory(MemoryAccess(0x7fffffffe146, 8))
-addr = 0x55555555514d
-while addr in code:
+	
 	inst = Instruction()
-	inst.setOpcode(code[addr])
-	inst.setAddress(addr)
+	inst.setOpcode(instruction.opcode)
+	inst.setAddress(instruction.addr)
 	ctx.processing(inst)
 	if inst.isTainted():
-		print('[tainted] %s' % str(inst))
-	else:
-		print('[*] %s' % str(inst))
-	addr = ctx.getConcreteRegisterValue(ctx.registers.rip)
+		if args.from_addr and args.from_addr > instruction.addr or args.to_addr and instruction.addr > args.to_addr:
+			return
+		if args.from_takt and args.from_takt > instruction.takt or args.to_takt and instruction.takt > args.to_takt:
+			return
+		print('[taint] %s' % str(inst))
+	#next = ctx.getConcreteRegisterValue(ctx.registers.rip)
+
+memory = {}
+def save_mem(addr, mem):
+	global memory
+	for i in range(len(val)):
+		memory[addr+i] = mem[i]
+
+def find_mem(addr, needle, size):
+	was_found = False
+	high_boundary_search = addr + size
+	while addr <= high_boundary_search:
+		for i in range(len(needle)):
+			value = memory.get(addr + i)
+			if value == None:
+				return None
+			if needle.find( chr(value) ) == -1:
+				was_found = False
+				addr += 1
+				break
+			else:
+				was_found = True
+		if was_found:
+			return addr
+	return None
+
+def align(size):
+	return ((size >> 2) +1) << 2
+
+class Step:
+	def __init__(self):
+		self.mem = {}
+	def mem_read(self, addr, val):
+		self.mem[addr] = val
+	def mem_write(self, addr, val):
+		self.mem[addr] = val
+	def set_registers(self, regs):
+		(self.rax,self.rcx,self.rdx,self.rbx,self.rsp,self.rbp,self.rsi,self.rdi) = regs
+
+
+parser = argparse.ArgumentParser( description='data flow analisys tool' )
+parser.add_argument("tracefile", type=str, help="trace.log")
+parser.add_argument("-taint_data", type=str, default='', help='taint data: "GET / HTTP/1.1" or input.bin')
+parser.add_argument("-from_takt", type=int, default=0, help="print tainted instruction only after takt")
+parser.add_argument("-to_takt", type=int, default=0, help="print tainted instruction only before takt")
+parser.add_argument("-from_addr", type=int, default=0, help="print tainted instruction only from address")
+parser.add_argument("-to_addr", type=int, default=0, help="print tainted instruction only to address")
+args = parser.parse_args()
+
+if path.isfile(args.taint_data):
+	with open(args.taint_data) as f:
+		args.taint_data = f.read()
+
+instruction = None
+has_tainted_data = False
+tainted_memory = set()
+with open(args.tracefile, 'r') as f:
+	while True:
+		line = f.readline()
+		if not line:
+			break
+		if line.startswith('['):
+			continue
+
+		line = line.split('\n')[0]
+		#print(line)
+		if line.find('{') != -1:
+			addr,opcode,regs = line.split(' ')
+			opcode = codecs.decode(opcode[1:-1],'hex')
+			rax,rcx,rdx,rbx,rsp,rbp,rsi,rdi = map(lambda r:int(r,16), regs.split(','))
+			if instruction and has_tainted_data:
+				try:
+					emulate(instruction)
+				except Exception as e:
+					#print(hex(instruction.addr))
+					#print(str(e))
+					pass
+			instruction = Step()
+			instruction.set_registers((rax,rcx,rdx,rbx,rsp,rbp,rsi,rdi))
+			instruction.opcode = opcode
+			icount,addr,thread = addr.split(':')
+			instruction.takt = int(icount)
+			if instruction.takt % 10000 == 0:
+				print("[*] %d" % instruction.takt)
+			instruction.addr = int(addr, 16)
+		elif line.find('[') != -1:
+			if line.find('-') != -1:
+				addr,mem,access,val = line.split(' ')
+				mem = int(mem[1:-1], 16)
+				size = len(val[2:])
+				if not val:
+					continue
+				val = int(val, 16)
+				if size == 2: 
+					val = pack('B', val)
+				elif size == 4:
+					val = pack('<H', val)
+				elif size == 8:
+					val = pack('<L', val)
+				elif size == 16:
+					val = pack('<Q', val)
+				else:
+					val = None
+				if val:
+					if access == '->':
+						instruction.mem_read(mem, val)
+					elif access == '<-':
+						instruction.mem_write(mem, val)
+			elif line.find(':') != -1:
+				addr,mem,val = line.split(' ')
+				mem = int(mem[1:-2], 16)
+				val = codecs.decode(val,'hex')
+				save_mem(mem,val)
+				ptr = find_mem(mem, args.taint_data, len(val))
+				if ptr and not ptr in tainted_memory:
+					print("[*] 0x%08x taint memory 0x%08x" % (instruction.addr,ptr))
+					ctx.taintMemory(MemoryAccess(ptr, align(len(args.taint_data))))
+					has_tainted_data = True
+					tainted_memory.add(ptr)
